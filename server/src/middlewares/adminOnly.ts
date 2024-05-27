b08d77c15713b92b0d47d69313ebdab482da8075
Leonite1 <122058216+Leonite1@users.noreply.github.com>
2024-05-27 23:15:54 +0100

    Update adminOnly.ts

--- server/src/middlewares/adminOnly.ts
+++ server/src/middlewares/adminOnly.ts
@@ -2,23 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 
 const adminOnly = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        // Get reference to the database from the Express app
         const db = req.app.get('db');
 
-        // Find the user in the database using their ID
         const user = await db.collection('users').findOne({ _id: req.userId });
 
-        // If user is found and has admin role, call the next middleware function
         if (user && user.role === 'admin') {
             next();
         } else {
-            // If user is not found or does not have admin role, send 403 Forbidden response
             res.status(403).json({
                 errors: { global: 'You are unauthorized for this action'}
             });
         }
     } catch (err) {
-        // Handle database errors
         console.error(err: );
         res.status(500).json({ errors: { global: err.message }});
     }
